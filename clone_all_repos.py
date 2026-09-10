61493221c356f57bbe3c70c356cc2c335fb72a57
MoUsT4Ki <noe1.marchal@epitech.eu>
2026-09-10 15:47:59 +0200

    Refactor repository cloning logic for improved readability and error handling
    and had code for people have more than 30 repos in organization

--- clone_all_repos.py
+++ clone_all_repos.py
@@ -2,33 +2,39 @@ import os
 import requests
 import subprocess
 
-token = "ACCESS_TOKEN"
+token = "YOUR_TOKEN_HERE"
 org_name = "ORG_NAME"
 headers = {"Authorization": f"Bearer {token}"}
-api_url = f"https://api.github.com/orgs/{org_name}/repos?type=all" 
 
 if not os.path.exists(org_name):
     os.makedirs(org_name)
 
-response = requests.get(api_url, headers=headers)
-print("Status Code:", response.status_code)
-print("Response JSON:", response.json())
+page = 1
+all_repos = []
 
-if response.status_code == 200:
-    repos = response.json()
-    if repos:
-        for repo in repos:
-            repo_name = repo['name']
-            clone_url = repo['clone_url']
+while True:
+    api_url = f"https://api.github.com/orgs/{org_name}/repos?type=all&per_page=100&page={page}"
+    response = requests.get(api_url, headers=headers)
+    if response.status_code != 200:
+        print("Error:", response.status_code, response.json())
+        break
+    repos_page = response.json()
+    if not repos_page:
+        break
+    all_repos.extend(repos_page)
+    page += 1
 
-            print(f"Cloning {repo_name}...")
+print(f"Number of repos find : {len(all_repos)}")
 
-            repo_path = os.path.join(org_name, repo_name)
-            if not os.path.exists(repo_path):
-                subprocess.run(["git", "clone", clone_url, repo_path])
-            else:
-                print(f"The repository {repo_name} already exists.")
-    else:
-        print("No repositories found in the organization.")
+if all_repos:
+    for repo in all_repos:
+        repo_name = repo['name']
+        clone_url = repo['clone_url'].replace("https://", f"https://{token}@")
+        print(f"Cloning {repo_name}...")
+        repo_path = os.path.join(org_name, repo_name)
+        if not os.path.exists(repo_path):
+            subprocess.run(["git", "clone", clone_url, repo_path])
+        else:
+            print(f"The repository {repo_name} already exists.")
 else:
-    print("Error:", response.status_code, response.json())
+    print("No repositories found in the organization.")
